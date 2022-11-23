ae71622278f67032144dac15aeb2bbf8c92e79de
Iofakh <it.khramov@gmail.com>
2022-11-23 16:09:16 +0300

    3rd part of 1st task

--- synergytphw3_5cpp_pt1.cpp
+++ synergytphw3_5cpp_pt1.cpp
@@ -15,5 +15,13 @@ main()
         printf("%i\n", j++);
     }
 
-
+    int k = 1;
+    int n;
+    printf("Enter the number up to which you want to display odd numbers, not including: ");
+    scanf("%i",&n);
+    while(k < n)
+    {
+        printf("%i\n", k++);
+        k++;
+    }
 }
